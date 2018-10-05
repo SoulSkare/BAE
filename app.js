ee5fd5fb9a2b0403fe6ff6dd86dc23ad5e618282
kieronwel <kieron.welman@gmail.com>
2018-10-05 21:31:30 +0700

    Quenum settings

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fs = require('fs')
 var lineReader = require('line-reader');
 const BAEmailScraper = require("./controllers/scraper.js");
 // const mongoose = require('mongoose')
-// const mongo_url = 'mongodb://admin:Z9PebEmail@ds113873.mlab.com:13873/emaildb'
+
 // mongoose.Promise = global.Promise
 // mongoose.connect(mongo_url, {useNewUrlParser: true})
 // mongoose.connection.on('error', err => {
@@ -69,7 +69,51 @@ async function run(){
 	//   }
 	// });
 
+	function get_line(filename, line_no, callback) {
+	    fs.readFile(filename, function (err, data) {
+	      if (err) throw err;
 
+	      // Data is a buffer that we need to convert to a string
+	      // Improvement: loop over the buffer and stop when the line is reached
+	      var lines = data.toString('utf-8').split("\n");
+
+	      if(+line_no > lines.length){
+	        return callback('File end reached without finding line', null);
+	      }
+
+	      callback(null, lines[+line_no]);
+	    });
+	}
+
+	let getQueNum = get_line('queuenum.txt', 1, (err, line) => {
+		return line
+	})
+
+	setInterval(() => {
+		get_line('./genkeywords.txt', getQueNum, async function(err, line){
+			getQueNum = get_line('queuenum.txt', 1, (err, line) => {
+				return line
+			})
+
+			console.log('The line: ' + line);
+			let searchQuery = `${line} "@gmail.com" "@outlook.com"`
+			await BAEmailScraper.start(searchQuery, 1, 3, "myEmailList.txt"); 
+
+			let incQue = getQueNum + 1
+
+			fs.writeFile("queuenum.txt", String(incQue), (err) => {
+			    if(err) {
+			        return console.log(err);
+			    }
+			    console.log("The quenum was saved!");
+			}); 
+		})
+	}, 60000
+
+
+	// fs.writeFile('temp.txt', (err, buff) => {
+	// 	console.log(buff.toString())
+	// })
 
 	// or read line by line:
 	lineReader.open('genkeywords.txt', function(err, reader) {
